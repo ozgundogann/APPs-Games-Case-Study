a9e5a8b5f0aa9b3dd0e6e5ee71a00e3b42a1724a
ozgunndogan <ozgundogan2000@gmail.com>
2025-01-08 17:44:42 +0300

    Add GameManager and GameStates

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GameStates currentGameState;
+
+    public event Action<GameStates> OnGameStateChange;
+    
+    public static GameManager Instance { get; private set; }
+
+    private void Awake() 
+    { 
+        if (Instance != null && Instance != this) 
+        { 
+            Destroy(this); 
+        } 
+        else 
+        { 
+            Instance = this; 
+        } 
+    }
+
+    public void ChangeGameState(GameStates newState)
+    {
+        if (currentGameState == newState) return;
+        
+        OnGameStateChange?.Invoke(newState);
+
+        currentGameState = newState;
+    }
+}
---
+++ Assets/Scripts/GameStates.cs
@@ -0,0 +1,6 @@
+﻿public enum GameStates
+{
+    LOBBY,
+    INGAME,
+    GAMEOVER
+}
