2809225c7822d7a440d6fd1f8b0433c2283c9e3a
ozgunndogan <ozgundogan2000@gmail.com>
2025-01-09 21:58:13 +0300

    Added Object Pooling

---
+++ Assets/Scripts/PoolManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    [SerializeField] private GameObject platformPrefab; 
+    [SerializeField] private int poolSize = 4;
+    private Queue<GameObject> platformPool = new Queue<GameObject>();
+
+    private void Awake()
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            var platform = Instantiate(platformPrefab, transform);
+            platform.SetActive(false); 
+            platformPool.Enqueue(platform);
+        }
+
+        Debug.Log(platformPool.Count);
+    }
+
+    public GameObject GetPlatform()
+    {
+        var platform = platformPool.Dequeue();
+        platform.SetActive(true);
+        platformPool.Enqueue(platform);
+        return platform;
+    }
+}
---
+++ Assets/Scripts/SectionPositioner.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class SectionPositioner : MonoBehaviour
+{
+    [SerializeField] private PoolManager poolManager;
+    [SerializeField] private Transform platformTransform;
+    
+    [SerializeField] private Transform player; 
+    [SerializeField] private int initialPlatforms = 5;
+
+    private float platformLength;
+    private Vector3 nextSpawnPosition;
+    private Coroutine newRoutine;
+    private void Start()
+    {
+        platformLength = platformTransform.localScale.z;
+        
+        for (int i = 0; i < initialPlatforms; i++)
+        {
+            SpawnPlatform();
+        }
+
+        newRoutine = StartCoroutine(RepositionPlatform());
+    }
+
+    private void OnDestroy()
+    {
+        StopCoroutine(newRoutine);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            SpawnPlatform();
+        }
+    }
+
+    private IEnumerator RepositionPlatform()
+    {
+        if (player.position.z > nextSpawnPosition.z - (platformLength * initialPlatforms / 2))
+        {
+            SpawnPlatform();
+        }
+
+        yield return null;
+    }
+
+    private void SpawnPlatform()
+    {
+        var platform = poolManager.GetPlatform();
+        platform.transform.position = nextSpawnPosition;
+        nextSpawnPosition += Vector3.forward * platformLength; 
+    }
+}
