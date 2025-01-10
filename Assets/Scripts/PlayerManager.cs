8c51e3ce12b540d7ff3e2bc4386aa249acf385b3
ozgunndogan <ozgundogan2000@gmail.com>
2025-01-10 21:07:30 +0300

    Add player manager

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,7 +1,63 @@
-﻿namespace DefaultNamespace
+﻿using System;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
 {
-    public class PlayerManager
+    [SerializeField] private CharacterMovement characterMovement;
+    [SerializeField] private RotateMovement rotateMovement;
+    [SerializeField] private FlyMovement flyMovement;
+
+    private void OnEnable()
     {
-        
+        GameManager.OnGameStateChange += ListenGameStates;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.OnGameStateChange -= ListenGameStates;
+    }
+
+    private void ListenGameStates(GameStates newState)
+    {
+        switch (newState)
+        {
+            case GameStates.NONE:
+                break;
+            case GameStates.STARTGAME:
+                HandleStartGame();
+                break;
+            case GameStates.INGAME:
+                break;
+            case GameStates.GAMEOVER:
+                HandleGameOver();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+    }
+
+    private void HandleStartGame()
+    {
+        EnableScripts();
+    }
+
+    private void EnableScripts()
+    {
+        characterMovement.enabled = true;
+        rotateMovement.enabled = true;
+        flyMovement.enabled = true;
+    }
+
+    private void HandleGameOver()
+    {
+        DisableScripts();
+    }
+
+    private void DisableScripts()
+    {
+        characterMovement.enabled = false;
+        rotateMovement.enabled = false;
+        flyMovement.enabled = false;
+        Debug.Log("Disable");
     }
 }
