007df4cd17637b56951c6dd2aa0966e165b64864
ozgunndogan <ozgundogan2000@gmail.com>
2025-01-11 16:53:16 +0300

    Add UI added

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject looseScreen;
+    
+    private void OnEnable()
+    {
+        GameManager.OnGameStateChange += ListenGameStates;
+    }
+
+    private void ListenGameStates(GameStates newState)
+    {
+        switch (newState)
+        {
+            case GameStates.NONE:
+                break;
+            case GameStates.INGAME:
+                looseScreen.SetActive(false);
+                break;
+            case GameStates.GAMEOVER:
+                HandleGameOver();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+    }
+
+    private void HandleGameOver()
+    {
+        looseScreen.SetActive(true);
+    }
+}
