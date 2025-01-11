eaf9b28aec7f3b331cce58c1b00aa6f5c934571c
ozgunndogan <ozgundogan2000@gmail.com>
2025-01-11 16:53:30 +0300

    Add Bounce added

--- Assets/Scripts/CharacterCollision.cs
+++ Assets/Scripts/CharacterCollision.cs
@@ -4,22 +4,44 @@ using UnityEngine;
 
 public class CharacterCollision : MonoBehaviour
 {
+    
     [SerializeField] private CharacterMovement characterMovement;
-    [SerializeField] private RotateMovement rotateMovement;
+
+    [SerializeField] private float bounceForce = 10f;
     
     [SerializeField] private Rigidbody rb;
-
     
+    private bool isGameOver;
+    private void OnEnable()
+    {
+        isGameOver = false;
+    }
+
+    private void OnDisable()
+    {
+        isGameOver = true;
+    }
+
     private void OnCollisionEnter(Collision other)
     {
+        //if(isGameOver) return;
         if (other.gameObject.TryGetComponent<Platform>(out var platform))
         {
             platform.PlatformResponsibility(characterMovement);
             return;
         }
         
-        Debug.Log("GAME OVER");
-        GameManager.Instance.TriggerGameOver();
+        
+        ContactPoint contact = other.contacts[0];
+        Vector3 bounceDirection = Vector3.Reflect(characterMovement.Velocity, contact.normal).normalized;
+
         rb.useGravity = true;
+        if(bounceForce < 0.01) return;
+        bounceForce *= 0.5f;
+        rb.AddForce(bounceDirection * bounceForce, ForceMode.Impulse);
+        Debug.DrawRay(transform.position,bounceDirection, Color.red);
+        isGameOver = true;
+        GameManager.Instance.TriggerGameOver();
+
     }
 }
